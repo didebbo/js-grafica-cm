c8cd96b1f75ca2561b0cd6817757f55ee2531107
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-01 20:15:58 +0200

    completato algoritmo square

--- js/index.js
+++ js/index.js
@@ -3,14 +3,40 @@ const grid = document.getElementById("grid");
 
 // Functions
 
+const isDiv = (num) => {
+
+    let i = 0;
+    do {
+        let a = num / i;
+        if (a == i) return true;
+        i++;
+    }
+    while (i <= num);
+    return false;
+}
+
+const numDiv = (num) => {
+
+    let i = 0;
+    do {
+        let a = num / i;
+        if (a == i) return i;
+        i++;
+    }
+    while (i <= num);
+}
+
 const genGrid = (num) => {
 
-    let totalCells = num * num;
+    // let totalCells = num * num;
 
-    for (let i = 1; i <= totalCells; i++) {
+    console.log(isDiv(num));
+
+    for (let i = 1; i <= num; i++) {
         let cell = document.createElement("li");
         cell.innerHTML = i;
-        grid.style.gridTemplateColumns = "repeat(" + num + ",1fr)";
+        if (isDiv(num)) grid.style.gridTemplateColumns = "repeat(" + numDiv(num) + ",1fr)";
+        else if (num <= 10) grid.style.gridTemplateColumns = "repeat(" + num + ",1fr)";
         grid.appendChild(cell);
 
         // Aggiunto evento click
@@ -26,7 +52,7 @@ const main = () => {
     let num;
 
     do num = parseInt(prompt("Inserisci numero celle (minino 1, massimo 10)"))
-    while (isNaN(num) || num < 1 || num > 10);
+    while (isNaN(num) || num < 1);
 
 
     genGrid(num);

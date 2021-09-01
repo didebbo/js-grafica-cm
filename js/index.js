91fc98296daec9c7b927b6ee5f64fba1d8a66b6d
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-01 15:15:22 +0200

    aggiunta struttura html e css.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("init");
+// DOM
+const grid = document.getElementById("grid");
+
+// Vars
+let num;
+
+
+
+do num = parseInt(prompt("Inserisci numero celle (minino 1, massimo 50)"))
+while (isNaN(num) || num < 1 || num > 50);
+

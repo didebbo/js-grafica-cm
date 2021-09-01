3e984864d9200a8b41a164de4174f86b0e47b8f8
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-01 15:51:30 +0200

    aggiunta funzione che genera celle

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 // DOM
 const grid = document.getElementById("grid");
 
-// Vars
-let num;
+// Functions
+
+const genGrid = (num) => {
+
+    for (let i = 1; i <= num; i++) {
+        let cell = document.createElement("li");
+        let content = document.createElement("div");
+        cell.appendChild(content);
+        content.innerHTML = i;
+        // console.log(cell);
+        grid.appendChild(cell);
+    }
+}
+
+const main = () => {
+    // Vars
+    let num;
+
+    do num = parseInt(prompt("Inserisci numero celle (minino 1, massimo 50)"))
+    while (isNaN(num) || num < 1);
+
+    genGrid(num);
+}
 
 
-
-do num = parseInt(prompt("Inserisci numero celle (minino 1, massimo 50)"))
-while (isNaN(num) || num < 1 || num > 50);
-
+// Main
+main();
